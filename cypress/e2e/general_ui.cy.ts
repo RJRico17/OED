031680997a227385393b30a6109c978444b5ff46
aravindb24 <aravindb2004@gmail.com>
2024-11-22 16:27:38 -0500

    Intiial form tests

--- cypress/e2e/general_ui.cy.ts
+++ cypress/e2e/general_ui.cy.ts
@@ -12,5 +12,29 @@ describe('UI Functionality Tests for Open Energy Dashboard', () => {
         cy.wrap(button).click({ force: true }); // Test click
       });
     });
+    
+    it('Tests all form inputs', () => {
+        // Test text and email inputs
+        cy.get('input[type="text"], input[type="email"]').each((input) => {
+          cy.wrap(input).should('be.visible').type('Sample text'); // Check visibility and type
+        });
+    
+        // Test password inputs
+        cy.get('input[type="password"]').each((input) => {
+          cy.wrap(input).should('be.visible').type('SamplePassword123');
+        });
+    
+        // Test textareas
+        cy.get('textarea').each((textarea) => {
+          cy.wrap(textarea).should('be.visible').type('Sample description text');
+        });
+    
+        // Submit forms
+        cy.get('form').each((form) => {
+          cy.wrap(form).within(() => {
+            cy.get('button[type="submit"], input[type="submit"]').click({ force: true });
+          });
+        });
+      });
   });
   
