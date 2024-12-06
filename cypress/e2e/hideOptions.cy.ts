85ba9f5aad5cfe30e5fbb340b4595623bbc87931
Gowrisankar Palanickal <156338043+shankarp05@users.noreply.github.com>
2024-12-06 15:48:20 -0500

    adding to hide options button test

--- cypress/e2e/hideOptions.cy.ts
+++ cypress/e2e/hideOptions.cy.ts
@@ -11,6 +11,12 @@ describe("Options Menu Tests", () => {
     // Click "Hide options"
     cy.get("#header > div > div.col-4.justify-content-end.d-lg-flex.d-none > div > nav > div > ul > li.dropdown.show.nav-item > div > button:nth-child(2)").click();
 
+    // Verify that graph configuration options are hidden
+    cy.get("#header > div > div.col-4.justify-content-end.d-lg-flex.d-none > div > nav > div > ul > li:nth-child(2) > a").should("not.exist"); // pages
+    cy.get("#header > div > div.col-4.justify-content-end.d-lg-flex.d-none > div > nav > div > ul > li:nth-child(3) > a").should("not.exist"); // options
+    cy.get("#header > div > div.col-4.justify-content-end.d-lg-flex.d-none > div > nav > div > ul > a:nth-child(4)").should("not.exist"); // help
+    
+
     
 
   });
