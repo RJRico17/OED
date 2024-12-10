8f7b666ab0b90a0ba365e4a2adf36dd5ac646275
Gowrisankar Palanickal <156338043+shankarp05@users.noreply.github.com>
2024-12-10 15:28:14 -0500

    adding to groups test

--- cypress/e2e/pagesGroups.cy.ts
+++ cypress/e2e/pagesGroups.cy.ts
@@ -1,3 +1,16 @@
 describe("Navigation to Groups Page", () => {
+  beforeEach(() => {
+    // Visit the application home page
+    cy.visit("/");
+  });
 
+  it("should navigate to the Groups page when 'Groups' is clicked from the Pages dropdown", () => {
+    // Open the Pages dropdown
+    cy.get("#header > div > div.col-4.justify-content-end.d-lg-flex.d-none > div > nav > div > ul > li:nth-child(2) > a").click();
+
+    // Click the 'Groups' option in the dropdown
+    cy.contains("Groups").click();
+
+
+  });
 });
