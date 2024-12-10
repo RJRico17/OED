ee99424981676dc0f868d3b4467081f75a8381f8
Gowrisankar Palanickal <156338043+shankarp05@users.noreply.github.com>
2024-12-10 15:34:30 -0500

    starting meters button testing

--- cypress/e2e/pagesMeters.cy.ts
+++ cypress/e2e/pagesMeters.cy.ts
@@ -1,4 +1,13 @@
 describe("Navigation to Meters Page", () => {
+  beforeEach(() => {
+    // Visit the application home page
+    cy.visit("/");
+  });
 
-  
+  it("should navigate to the Meters page when 'Meters' is clicked from the Pages dropdown", () => {
+    // Open the Pages dropdown
+    cy.get("#header > div > div.col-4.justify-content-end.d-lg-flex.d-none > div > nav > div > ul > li:nth-child(2) > a").click();
+
+
+  });
 });
