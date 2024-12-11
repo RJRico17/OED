0880bc14153581a6ea4ab26ab82adce567a10adb
Gowrisankar Palanickal <156338043+shankarp05@users.noreply.github.com>
2024-12-10 22:26:14 -0500

    beginning tooltip icon testing

---
+++ cypress/e2e/tooltipIcon.cy.ts
@@ -0,0 +1,9 @@
+describe('Tooltip Attribute Test', () => {
+    it('should add aria-describedby attribute after clicking the element', () => {
+      // Visit the page
+      cy.visit('/');
+  
+
+    });
+  });
+  
