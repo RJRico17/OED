c628d533123af968f738772a12637b4c380fa242
Gowrisankar Palanickal <156338043+shankarp05@users.noreply.github.com>
2024-12-10 22:31:37 -0500

    finishing tooltip test

--- cypress/e2e/tooltipIcon.cy.ts
+++ cypress/e2e/tooltipIcon.cy.ts
@@ -8,7 +8,12 @@ describe('Tooltip Attribute Test', () => {
         .should('exist') // Ensure the element exists
         .and('not.have.attr', 'aria-describedby'); // Verify aria-describedby is not present initially
   
-
+      // Click the element
+      cy.get('i[data-for="all"][data-tip="help.home.navigation"]').click();
+  
+      // Verify aria-describedby is added after clicking
+      cy.get('i[data-for="all"][data-tip="help.home.navigation"]')
+        .should('have.attr', 'aria-describedby', 'all'); // Confirm the correct attribute value
     });
   });
   
