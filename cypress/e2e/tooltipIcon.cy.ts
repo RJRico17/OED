a70d62729755f442930a542fb5cccd5988418d6d
Gowrisankar Palanickal <156338043+shankarp05@users.noreply.github.com>
2024-12-10 22:29:26 -0500

    adding to tooltip icon test

--- cypress/e2e/tooltipIcon.cy.ts
+++ cypress/e2e/tooltipIcon.cy.ts
@@ -3,6 +3,11 @@ describe('Tooltip Attribute Test', () => {
       // Visit the page
       cy.visit('/');
   
+      // Locate the element with the tooltip
+      cy.get('i[data-for="all"][data-tip="help.home.navigation"]')
+        .should('exist') // Ensure the element exists
+        .and('not.have.attr', 'aria-describedby'); // Verify aria-describedby is not present initially
+  
 
     });
   });
