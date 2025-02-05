4944d67939c3a5fb7177c12c67e1eca6c01b8a93
Steven Huss-Lederman <huss@beloit.edu>
2025-02-05 11:53:02 -0600

    remove test add comment for ones done but leftover

--- src/server/test/web/readingsBarGroupQuantity.js
+++ src/server/test/web/readingsBarGroupQuantity.js
@@ -201,7 +201,6 @@ mocha.describe('readings API', () => {
 
                 });
 
-                // Add BG9 here
                 mocha.it('BG9: 1 day bars for 15 + 20 minute reading intervals and quantity units with +-inf start/end time & kWh as MJ reverse conversion', async () => {
 
                     const unitData = unitDatakWh.concat([
